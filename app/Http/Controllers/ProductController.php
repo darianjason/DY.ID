29581832100e84aaab5262893cfaed8cb8c4db2d
Darian Jason <darianjason@gmail.com>
2021-12-28 20:11:10 +0700

    Add ProductController.php

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Detail;
+
+class ProductController extends Controller
+{
+    public function all()
+    {
+        $categories = Category::all();
+        $products = Product::all();
+        $details = Detail::all();
+
+        return view('index', compact('categories', 'products', 'details'));
+    }
+}
