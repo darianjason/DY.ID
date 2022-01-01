185925f9c5b52e797d05328e69f61b85b0f2b8d7
Darian Jason <darianjason@gmail.com>
2022-01-01 23:27:22 +0700

    Add form id attribute and input labels

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,13 +5,18 @@
 @section('content')
     <h2>Log In</h2>
 
-    <form action="login" method="POST">
+    <form action="login" method="POST" id="login-form">
         @csrf
 
         <label for="email">Email</label>
         <input type="email" name="email" id="email" placeholder="user@example.com" value="{{ Cookie::get('emailCookie') != null ? Cookie::get('emailCookie') : '' }}">
+
+        <label for="password">Password</label>
         <input type="password" name="password" id="password" value="{{ Cookie::get('passwordCookie') != null ? Cookie::get('passwordCookie') : '' }}">
-        <input type="checkbox" name="remember" id="remember" {{ Cookie::get('emailCookie') != null ? 'checked' : '' }}>Remember Me
+
+        <input type="checkbox" name="remember" id="remember" {{ Cookie::get('emailCookie') != null ? 'checked' : '' }}>
+        <label for="remember">Remember Me</label>
+
         <input type="submit" value="Log In">
     </form>
 
