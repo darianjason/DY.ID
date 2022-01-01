db8648e923d4e2bc05d8c4a8c007dfbcc52eddb7
Darian Jason <darianjason@gmail.com>
2022-01-01 21:24:19 +0700

    Remove Role model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,11 +42,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
-
     public function cart()
     {
         return $this->hasOne(Cart::class);
